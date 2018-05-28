d5f4a20b95f3afdbdf5f81e9cdc1af6de016f419
emisia <emisia@dt2014013.emisia.lan>
2018-05-28 12:34:10 +0200

    WS-2018.1 <emisia@dt2014013.emisia.lan Create baseRefactoring.xml, IntelliLang.xml, ide.general.xml, sencha.xml, project.default.xml, diff.xml, other.xml, Default.xml, keymap.xml, find.xml, ui.lnf.xml, projectView.xml, ExtJS Test class.js, code.style.schemes.xml, colors.scheme.xml, External Tools.xml, notifications.xml, nemanja.icls, Dule_custom.xml, customization.xml, Default.xml, JavaScript.xml, parameter.hints.xml, jdk.table.xml, laf.xml, Eclipse copy.xml, Default _1_.xml, web-browsers.xml, ExtJS Formater.xml, vcs.xml, editor.xml, Emisia.xml, usageView.xml, hg.xml, debugger.xml, path.macros.xml

---
+++ fileTemplates/ExtJS Test class.js
@@ -0,0 +1,15 @@
+Ext.define('TEST.Connection.', {
+    extend: 'TEST.Abstract',
+    singleton: true,
+    p_jsonFileNames: [''],
+    p_mocks: {
+    },
+    p_noMocks: [],
+    m_runTests: function(){
+        var me = this;
+
+        describe('Main test', function(){
+        
+        });
+    }
+});
